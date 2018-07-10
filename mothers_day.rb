a8fc33460c6e8e356699da547ec365cbabad8df1
Kelly Cotter <kjc853@gmail.com>
2018-07-10 17:55:36 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,7 @@
-## Define your method, mothers_day, below. Go through the README and update your method as needed!
+# Define your method, mothers_day, below. Go through the README and update your method as needed!
 
 
+def mothers_day(moms_name="Beyonce")
+  puts "Happy Mother's Day, #{moms_name}!"
+end
+mothers_day
